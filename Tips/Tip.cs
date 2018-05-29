ec721778667ad7ce72fa1096900faabeabfd83ec
Steve Conger <steve@spconger.com>
2018-05-29 15:04:47 -0700

    Add project files.

---
+++ Tips/Tip.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tips
+{
+    public class Tip
+    {
+        public double BillAmount { get; set; }
+        public double TipPercent { get; set; }
+        public double TaxPercent { get; set; }
+
+        public double CalculateTip()
+        {
+            if(TipPercent > 1)
+            {
+                TipPercent /= 100;
+            }
+            return BillAmount * TipPercent;
+        }
+
+        public double CalculateTax()
+        {
+            return BillAmount * TaxPercent;
+        }
+
+        public double CalculateTotal()
+        {
+            return BillAmount + CalculateTip() + CalculateTax();
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tips;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Tip t = new Tip();
+        [TestMethod]
+        public void CalcTipTest()
+        {
+            t.BillAmount = 100;
+            t.TipPercent = .20;
+            double tipAmount = t.CalculateTip();
+            Assert.AreEqual(tipAmount, 20);
+        }
+
+        [TestMethod]
+        public void CalcTotalTest()
+        {
+            t.BillAmount = 100;
+            t.TipPercent = .20;
+            t.TaxPercent = 9;
+            double taxAmount = t.CalculateTax();
+            Assert.AreEqual(t.CalculateTotal(), 129.00);
+        }
+    }
+}
